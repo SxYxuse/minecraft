11356c18299bbf667d177864d64245109c033a2e
Quentin Massart <quentinmassart93@gmail.com>
2023-02-05 14:51:01 +0100

    MANAGER : Update ChatEvent

--- manager/src/main/java/me/sxyxuse/manager/Manager.java
+++ manager/src/main/java/me/sxyxuse/manager/Manager.java
@@ -1,5 +1,8 @@
 package me.sxyxuse.manager;
 
+import me.sxyxuse.manager.listeners.players.ChatEvent;
+import me.sxyxuse.manager.listeners.players.JoinEvent;
+import me.sxyxuse.manager.listeners.players.QuitEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -7,10 +10,19 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.logging.Level;
 
 public class Manager extends JavaPlugin {
+    public static Manager MANAGER;
+
+    public static Manager getInstance() {
+        return MANAGER;
+    }
+
 
     @Override
     public void onEnable() {
+        MANAGER = this;
+
         this.registerListeners();
+
         this.log(Level.INFO, "Plugin démarré avec succès !");
     }
 
@@ -21,6 +33,9 @@ public class Manager extends JavaPlugin {
 
     private void registerListeners() {
         final PluginManager pm = Bukkit.getPluginManager();
+        pm.registerEvents(new JoinEvent(), this);
+        pm.registerEvents(new QuitEvent(), this);
+        pm.registerEvents(new ChatEvent(), this);
     }
 
     public void log(Level level, String message) {
